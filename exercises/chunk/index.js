6d5464b6ed33183923d57a1b5f89e3f61f943c5a
JairoHernandez <jairomh@hotmail.com>
2017-11-21 00:32:18 -0600

    Array Chunking

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -2,12 +2,48 @@
 // Given an array and chunk size, divide the array into many subarrays
 // where each subarray is of length size
 // --- Examples
-// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]] where 2 means  2 elements in each chunk.
 // chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
-// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]] where 4 means 4 elements in each chunk.
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    // SOLUTION 1
+    // // Create empty array to hold chunks called "chunked".
+    // const chunked = [];
+    
+    // // For each element in the "unchunked" array.
+    // for (let element of array ) {
+    //     // Retrieve the last element in unchunked array.
+    //     const last = chunked[chunked.length - 1];
+    //     console.log(`last: ${last} | element: ${element}`)
+        
+    //     // If last element does not exist, or if its length is equal to chunk size. 
+    //     if (!last || last.length === size) {
+    //         // Push a new chunk into 'chunked' with the current element.
+    //         chunked.push([element]);       
+    //     } else { // else add the current element into the chunk.
+    //         last.push(element);
+    //     }
+    // }
+    // return chunked;
 
+    // SOLUTION 2 way better
+    const chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+        console.log(`index: ${index} | arraylength: ${array.length}`);
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+    return chunked;
+    // index: 0 | arraylength: 5
+    // index: 2 | arraylength: 5
+    // index: 4 | arraylength: 5
+    // [ [ 1, 2 ], [ 3, 4 ], [ 5 ] ]
+}
+        
+console.log(chunk([1, 2, 3, 4, 5], 2));
 module.exports = chunk;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,34 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
 
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    // Build object.
+    for (let char of str) {
+
+        if (charMap[char]) { // if value exists increment it.
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+    console.log(charMap); // { H: 1, e: 3, l: 2, o: 1, ' ': 1, t: 1, h: 1, r: 1, '!': 1 }
+
+    // Iterate an object by using 'let char in'
+    for (let char in charMap) { // char is the key.
+        // console.log(char);
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char; // maxChar is now that character.
+        }
+    }
+    console.log(maxChar);
+    return maxChar;
+}
+
+maxChar('Hello there!');
 module.exports = maxChar;

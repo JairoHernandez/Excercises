5dda5627dde29c820a52a58fbc569497adfbfcc5
JairoHernandez <jairomh@hotmail.com>
2017-11-22 00:44:37 -0600

    Capitalization

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,18 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
 
+function cleanString(str) {
+  return str
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+}
+
+console.log(anagrams('rojai oocl', 'jairo cool'));
 module.exports = anagrams;
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,15 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const words = [];
 
+    for (let word of str.split(' ')) { // 'hi there buddy' --> ["hi","there","buddy"]
+        words.push(word[0].toUpperCase() + word.slice(1));
+    }
+    console.log(words); // [ 'Hola', 'Hombre', 'Viejo' ]
+    return words.join(' ');
+}
+
+console.log(capitalize('hola hombre viejo'));
 module.exports = capitalize;

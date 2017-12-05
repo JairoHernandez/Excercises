836494c163aa9bd70f1e3bbff89939fce1dfe1ca
JairoHernandez <jairomh@hotmail.com>
2017-12-05 17:28:25 -0600

    Sizing a List

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -13,6 +13,25 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head);  // this.head becomes new node.
+    }
+
+    size() { // Returns the number of nodes in the linked list.
+
+    }
 }
+
+const nodeOne = new Node(5);
+const list = new LinkedList();
+list.head = nodeOne; // {"data":5,"next":null}
+console.log(list.head);
+list.insertFirst(15);
+console.log(list); // {"head":{"data":15,"next":{"data":5,"next":null}}}
+for (let node in list.head) {
+    console.log('node:', node);
+}
+
 
 module.exports = { Node, LinkedList };

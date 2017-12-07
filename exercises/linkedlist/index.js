11309c104cf6954c05bb99be72ebba2db9fc98f3
JairoHernandez <jairomh@hotmail.com>
2017-12-06 23:56:49 -0600

    Adding InsertLast

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -4,6 +4,7 @@
 
 class Node {
     constructor(data, next=null) {
+        // object properties
         this.data = data;
         this.next = next;
     }
@@ -13,6 +14,7 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
 
     insertFirst(data) {
         this.head = new Node(data, this.head);  // data becomes new node, this.head is old node pushed to right.
@@ -52,6 +54,7 @@ class LinkedList {
         this.head = null; // We now have no reference to list of nodes who cares if it's floating in memory.
     }
 
+    // Move head to second node to get rid of first node. First node no longer has reference so it's like a deletion.
     removeFirst() {
         if (!this.head) {
             return null;
@@ -59,7 +62,42 @@ class LinkedList {
 
         this.head = this.head.next;
     }
-    // Move head to second node to get rid of first node. First node no longer has reference so it's like a deletion.
+    
+    // Temp value 'previous' points to what head points to and temp value 'node' points to next node. 
+    // Iterate till temp value node.next hits null to know we are at the end. Remove that node
+    // and mark 'previous' node with null.
+    removeLast() {
+        if (!this.head) { // removes the last node when list is empty
+            return; 
+        }
+
+        if (!this.head.next) { // if there's no second node.
+            this.head = null;
+            return;
+        }
+
+        // RemoveLast removes the last node when list is length 2 or more.
+        let previous = this.head;
+        let node = this.head.next;
+
+        while (node.next) { // see lecture 84 to make sense
+            previous= node;
+            node = node.next;
+        }
+        previous.next = null;
+    }
+
+    insertLast(data) {
+        const last = this.getLast();
+
+        if (last) {
+            // theere are some existing nodes in our chain.
+            last.next = new Node(data);
+        } else {
+            // The chain is empty so create a node.
+            this.head = new Node(data);
+        }
+    }
 }
 
 const nodeOne = new Node(5);

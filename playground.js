0fd57dd24dfdc06778938ba46b4ef04133af2aff
JairoHernandez <jairomh@hotmail.com>
2018-03-01 23:57:48 -0600

    Playground

--- playground.js
+++ playground.js
@@ -31,3 +31,32 @@ function foo2()
 
 console.log(foo1());
 console.log(foo2());
+
+console.log('+++++++++++++++++++++++++++++++++');
+
+var a = 'fcrxzwscanmligyxyvym';
+var b = 'jxwtrhvujlmrpdoqbisbwhmgpmeoke';
+
+var aclean = a.split('').sort().join('').trim();
+var bclean = b.split('').sort().join('').trim();
+
+var acleanArr = aclean.split('');
+var bcleanArr = bclean.split('');
+var count = 0;
+
+for (let index=0; index < bclean.length; index++) {
+    
+    if (acleanArr.indexOf(bcleanArr[index]) === -1) {
+        // console.log(bcleanArr[index] + ' not in acleanArr');
+        count += 1;
+    }
+}
+
+for (let index=0; index < aclean.length; index++) {
+
+    if (bcleanArr.indexOf(acleanArr[index]) === -1) {
+        // console.log(acleanArr[index] + ' not in bcleanArr');
+        count += 1;
+    }
+}
+console.log(count);
